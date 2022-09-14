c1c2ac0f36ce0ab40ffc8ba2aa82f686409f7dcb
SShreyaskunte <shreyas.kunte@tietoevry.com>
2022-09-14 12:25:18 +0530

    Commit 14/9/2022

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,104 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+//array - *ngfor
+//key-value
+Products:any[]=[
+  {
+    "productname" : "Kids Dress"
+  },
+  {
+    "productname" : "Kids toys"
+  },
+  {
+    "productname" : "Laptops"
+  },
+  {
+    "productname" : "Mobiles"
+  },
+  {
+    "productname" : "Watch"
+  },
+  {
+    "productname" : "Footwear"
+  },
+  {
+    "productname" : "Headphones"
+  },
+]
+
+//Event to hide products
+isVisible:boolean = true
+hide_products()
+{
+  this.isVisible =! this.isVisible;
+}
+
+// *ngSwitch
+product = "Laptop"
+
+//property errorcolor for style binding using ternary
+
+errorcolor:string='red'
+
+//array for style binding with ternary operator with ngSwitch
+ProductDetails:any[]=[
+  {
+    "pid":"100",
+    "pname":"Mobile",
+    "status":"Active",
+    "color":"green"
+  },
+  {
+    "pid":"101",
+    "pname":"Headphones",
+    "status":"InActive",
+    "color":"red"
+
+  },{
+    "pid":"102",
+    "pname":"Laptops",
+    "status":"Out Of Stock",
+    "color":"yellow"
+
+  },{
+    "pid":"103",
+    "pname":"Camera",
+    "status":"Active",
+    "color":"green"
+
+  },
+  {
+    "pid":"104",
+    "pname":"Kids toys",
+    "status":"Out Of Stock",
+    "color":"yellow"
+
+  },{
+    "pid":"105",
+    "pname":"Buses",
+    "status":"InActive",
+    "color":"red"
+
+  },{
+    "pid":"106",
+    "pname":"Shoes",
+    "status":"Active",
+    "color":"green"
+
+  },
+]
+
+//style binding using a fnunction
+
+getColor()
+{
+  return 'red';
+}
+ 
   ngOnInit(): void {
   }
+
+
 
 }
